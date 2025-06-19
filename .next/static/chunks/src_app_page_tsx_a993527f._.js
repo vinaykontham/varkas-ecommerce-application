8caa4ceb9de87e8205a44e6a35b55dc86bd89c51
vinaykontham <148061393+vinaykontham@users.noreply.github.com>
2025-06-19 22:39:23 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1750352962818

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_2b7e3fcc._.js",
+  "static/chunks/src_b25d83d7._.js"
+],
+    source: "dynamic"
+});
