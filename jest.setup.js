4d5e09d91c35d17cad40fc3dbef77ede03ceea70
vinaykontham <148061393+vinaykontham@users.noreply.github.com>
2025-06-19 22:38:39 +0530

    Add jest.setup.js 1750352918349

---
+++ jest.setup.js
@@ -0,0 +1,54 @@
+import '@testing-library/jest-dom'
+import { TextEncoder, TextDecoder } from 'util'
+
+// Suppress specific console warnings
+const originalError = console.error
+console.error = (...args) => {
+  if (args[0]?.includes('Received `true` for a non-boolean attribute `fill`')) {
+    return
+  }
+  originalError.apply(console, args)
+}
+
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text
+    return <img {...props} />
+  },
+}))
+
+// Mock next/navigation
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+      replace: jest.fn(),
+      prefetch: jest.fn(),
+      back: jest.fn(),
+    }
+  },
+  useSearchParams() {
+    return {
+      get: jest.fn(),
+    }
+  },
+}))
